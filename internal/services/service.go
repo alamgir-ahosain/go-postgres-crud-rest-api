24fb3a85d44474d6d8cc793f957ba4dfed84d9cf
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-19 00:31:11 +0600

    add return lind  in HandleHTTPError function

--- internal/services/service.go
+++ internal/services/service.go
@@ -3,19 +3,27 @@ package services
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
-	"github.com/alamgir-ahosain/go-postgres-crud-rest-api/internal/models"
+	"github.com/gorilla/mux"
 )
-
-
 
 func HandleHTTPError(w http.ResponseWriter, err error, code int) {
 	if err != nil {
 		http.Error(w, err.Error(), code)
+		return
 	}
 }
 
-func MakeJSONFormatFunc(w http.ResponseWriter,users []models.Users,code int){
+func MakeJSONFormatFunc(w http.ResponseWriter, data interface{}, code int) {
 	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(users)
+	json.NewEncoder(w).Encode(data)
+}
+
+func GetID(w http.ResponseWriter, r *http.Request) int {
+	ids := mux.Vars(r)
+	idStr := ids["id"] // map[string]string{"id":"5"}
+	id, err := strconv.Atoi(idStr)
+	HandleHTTPError(w, err, http.StatusBadRequest)
+	return id
 }
